27b668fd26b48536f0ff6f334489624db112ab43
IvanEstradaa <ivanestrada@live.com>
2025-02-24 09:37:29 -0600

    Added subtraction button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-function calculate() {
+function sum() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
 
     let result;
     result = Number(num1) + Number(num2);
+
+    document.getElementById('result').innerText = "Result: " + result;
+
+}
+
+function sub() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    let result;
+    result = Number(num1) - Number(num2);
 
     document.getElementById('result').innerText = "Result: " + result;
 
